c8523b43f3724a81d317d5190edd775b864e3f87
IlliaFedoruk <fedoruk07illia@gmail.com>
2024-01-13 13:20:39 +0100

    first commmit.2

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Арефметические знаки +, -, *, /.
+
+        int Box1 = 5;
+        int Box2 = 10;
+
+        int Box3 =584;
+        int Box4 =321;
+
+        int Box5 =14;
+        int Box6 =18;
+
+        int Box7 =735;
+        int Box8 =32;
+
+        //Сложение
+        System.out.println(Box1 + Box2);
+        //Вычитание
+        System.out.println(Box3 - Box4);
+        //Умножение
+        System.out.println(Box5 * Box6);
+        //Деление
+        System.out.println(Box7 / Box8);
+    }
+}
